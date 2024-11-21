c36321358c990cd76752008aafa9ccd6eb1d7ae1
datboi49 <datboifr1@gmail.com>
2024-11-21 00:10:49 -0500

    Removed redundant code + Debugging

--- GameDemo/src/objects/Enemy.java
+++ GameDemo/src/objects/Enemy.java
@@ -1,5 +1,7 @@
 package objects;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -11,13 +13,17 @@ public class Enemy extends Entity {
         super(x, y, width, height);
         this.target = target;
         this.speed = 1;
-        try { this.sprite = ImageIO.read(getClass().getResourceAsStream("/res/enemies/monster.png"));
-        } catch (IOException ex) {}
+        try {
+            this.sprite = ImageIO.read(getClass().getResourceAsStream("/res/enemies/monster.png"));
+        } catch (IOException ex) {
+        }
     }
 
-    public void moveTowardTarget(double delta) {
+    public void update(double delta) {
+        // Calculate direction vector to the target
         double directionX = (target.x + target.width / 2) - (this.x + this.width / 2);
         double directionY = (target.y + target.height / 2) - (this.y + this.height / 2);
+
         // Normalize direction
         double length = Math.sqrt((directionX * directionX) + (directionY * directionY));
         if (length != 0) {
@@ -25,10 +31,33 @@ public class Enemy extends Entity {
             directionY /= length;
         }
 
-        // Update position based on speed
-        x += directionX * speed;
-        y += directionY * speed;
-        // System.out.println("directionX: " + directionX + ", directionY: " +
-        // directionY);
+        // Scale by speed and delta time
+        double moveX = directionX * speed * delta;
+        double moveY = directionY * speed * delta;
+        this.x += moveX;
+        this.y += moveY;
+
+    }
+
+    public void draw(Graphics2D g) {
+        // Draw a placeholder rectangle if no sprite is set
+        g.setColor(Color.RED);
+        g.fillRect(x - (width / 2), y - (height / 2), width, height);
+
+        g.setColor(Color.WHITE);
+
+        double directionX = target.x - this.x;
+        double directionY = target.y - this.y;
+
+        g.drawLine(target.x, target.y, this.x, this.y);
+        g.drawString(directionX + " " + directionY, x - width, y - height);
+
+        // Normalize direction
+        double length = Math.sqrt((directionX * directionX) + (directionY * directionY));
+        if (length != 0) {
+            directionX /= length;
+            directionY /= length;
+        }
+
     }
 }
--- GameDemo/src/objects/Entity.java
+++ GameDemo/src/objects/Entity.java
@@ -5,21 +5,12 @@ import java.awt.image.BufferedImage;
 public class Entity extends Object {
 	int speed;
 	String direction;
-	BufferedImage[] animationFrames;
 	int spriteCounter = 0; // Counts frames for animation timing
 	int spriteIndex = 0;
 
 	// Constructor
 	public Entity(int x, int y, int width, int height) {
 		super(x, y, width, height);
-	}
-
-	// Set the animation frames for this entity
-	public void setAnimationFrames(BufferedImage[] frames) {
-		this.animationFrames = frames;
-		if (frames != null && frames.length > 0) {
-			sprite = frames[0]; // Default to the first frame
-		}
 	}
 
 }
