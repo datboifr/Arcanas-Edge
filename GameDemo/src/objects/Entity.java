1e3ba710cbd72d7a56fd2d25fa035c43f2e8728d
Anthony Campos <datboifr1@gmail.com>
2024-11-26 13:06:31 -0500

    Changing gameLoop to account for store

--- GameDemo/src/objects/Entity.java
+++ GameDemo/src/objects/Entity.java
@@ -4,7 +4,7 @@ public class Entity extends GameObject {
 	float speed;
 	String direction;
 	int spriteCounter = 0; // Counts frames for animation timing
-	int spriteIndex = 0;
+	//int spriteIndex = 0;
 	boolean attacking = false;
 	public int health;
 
